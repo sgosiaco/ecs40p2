c36933d4a188b10e66f02b8f5f9b553cd8d40473
sgosiaco <sgosiaco@me.com>
2016-04-09 02:03:52 -0700

    grepdir made

---
+++ grepdir.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+ARGS="-s "
+if [ $# -lt 2 ]; then
+  echo usage: $0 directory pattern [-grep option]*
+else
+  if [ ! -d "$1" ]; then
+    echo usage: $0 directory pattern [-grep option]*
+  else
+    if [ $# -eq 2 ]; then
+      find $1 -exec grep $ARGS $2 {} \;
+    else
+      shift
+      shift
+      while [ $# -gt 0 ]; do
+        if [[ $1 = \-* ]]; then
+          $ARGS+=$1
+        fi
+        shift
+      done
+      find $1 -exec grep $ARGS $2 {} \;
+    fi
+  fi
+fi
