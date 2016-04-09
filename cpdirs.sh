2970063b68c091683f97f38c577cc12126c7038f
Sean Peter Chua-Pao Gosiaco <sgosiaco@pc20.cs.ucdavis.edu>
2016-04-09 01:12:11 -0700

    Editted cpdirs.sh to work

--- cpdirs.sh
+++ cpdirs.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 if [ $# -ne 3 ]; then
-  echo usage: uncomp.sh \{filelist\}\+
+  echo usage: $0 source_directory1 source_directory2 dest_directory
 else
   if [ ! -d "$3" ]; then
-    mkdir $3
+    if [ ! -f "$3" ]; then
+      mkdir $3
+    else
+     echo usage: $0 source_directory1 source_directory2 dest_directory
+     exit
+    fi
   fi
-  cp $1/* $3/
+  cp -u $1/* $3/
   cp -u $2/* $3/
 fi
