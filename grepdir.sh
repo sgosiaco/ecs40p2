e4dc5c4579b555eae1c9add579cb227e72c6ca80
Sean Peter Chua-Pao Gosiaco <sgosiaco@pc20.cs.ucdavis.edu>
2016-04-09 02:17:48 -0700

    fixed grepdir

--- grepdir.sh
+++ grepdir.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 ARGS="-s "
+dir=""
+pattern=""
 if [ $# -lt 2 ]; then
   echo usage: $0 directory pattern [-grep option]*
 else
@@ -9,15 +11,20 @@ else
     if [ $# -eq 2 ]; then
       find $1 -exec grep $ARGS $2 {} \;
     else
+      dir=$1
+      pattern=$2
       shift
       shift
       while [ $# -gt 0 ]; do
         if [[ $1 = \-* ]]; then
-          ARGS+=$1
+          ARGS=$ARGS$1" "
+        else
+          echo usage: $0 directory pattern [-grep option]*
+          exit
         fi
         shift
       done
-      find $1 -exec grep $ARGS $2 {} \;
+      find $dir -exec grep $ARGS $pattern {} \;
     fi
   fi
 fi
