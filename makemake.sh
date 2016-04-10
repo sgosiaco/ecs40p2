d3338ec758f1209725d56113995cf16fa51215e2
Sean Gosiaco <scgosiaco@ucdavis.edu>
2016-04-10 00:15:57 -0700

    almost there

--- makemake.sh
+++ makemake.sh
@@ -5,6 +5,7 @@ exe=""
 files=""
 dep=""
 if [ $# -ne 1 ]; then
+  echo Executable name required.
   echo usage: makemake.sh executable_name
 else
   exe=$1
@@ -12,11 +13,11 @@ else
     files=$files${f%.cpp}".o "
   done
   echo $exe : $files >> Makefile
-  echo $'\t'$gpp -o $exe $files'\n' >> Makefile
+  echo $'\t'$gpp -o $exe $files$'\n' >> Makefile
   for file in *.cpp; do
-    dep=awk -F'"' '$0=$2' $file | awk 'BEGIN { ORS=" " }; /.h$/'
+    dep=$(awk -F'"' '$0=$2' $file | awk 'BEGIN { ORS=" " }; /.h$/')
     echo ${file%.cpp}".o " : $file $dep >> Makefile
-    echo $'\t'$gpp -c $file'\n' >> Makefile
+    echo $'\t'$gpp -c $file$'\n' >> Makefile
   done
-  echo clean :'\n'$'\t'rm -f $exe $files
+  echo clean :$'\n'$'\t'rm -f $exe $files >> Makefile
 fi
